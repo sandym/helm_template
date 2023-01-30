32275d99f7f3ef63a6873b3539066342c73d510b
sandy <sandy@mac.com>
2023-01-30 22:24:20 +1100

    formatting

--- src/index.ts
+++ src/index.ts
@@ -6,39 +6,42 @@
 		CHART={helm chart path} VALUES={values yaml} npm run watch
 */
 
-const chokidar = require('chokidar');
-const execSync = require('child_process').execSync;
-const fs = require('fs');
+const chokidar = require( 'chokidar' );
+const execSync = require( 'child_process' ).execSync;
+const fs = require( 'fs' );
 
 const hemlDir = process.env['CHART'];
 const values = process.env['VALUES'];
 
-const watcher = chokidar.watch(`${hemlDir}/**`, {
-	persistent: true
-});
+const watcher = chokidar.watch(
+	`${hemlDir}/**`,
+	{ persistent: true }
+);
 
 var inProgress = false;
-watcher.on('all',
-	async () =>
+watcher.on( 'all',
+	() =>
 	{
-		if (inProgress)
+		if ( inProgress )
 			return;
 		inProgress = true;
 
-		setTimeout(() =>
-		{
-			fs.writeFileSync( `${hemlDir}/../tmpl.yaml`, '' );
-			var cmd = 'helm template';
-			if (values)
+		setTimeout(
+			() =>
 			{
-				values.split(',').forEach( (v) => {
-					cmd += ` --values=${hemlDir}/${v}`;
-				});
-			}
-			cmd += ` ${hemlDir}/.`;
-			const tmpl = execSync( cmd ).toString();
-			fs.writeFileSync( `${hemlDir}/../tmpl.yaml`, tmpl );
-			inProgress = false;
-		}, 100 );
+				fs.writeFileSync( `${hemlDir}/../tmpl.yaml`, '' );
+				var cmd = 'helm template';
+				if (values)
+				{
+					values.split(',').forEach( ( v ) => {
+						cmd += ` --values=${hemlDir}/${v}`;
+					});
+				}
+				cmd += ` ${hemlDir}/.`;
+				const tmpl = execSync( cmd ).toString();
+				fs.writeFileSync( `${hemlDir}/../tmpl.yaml`, tmpl );
+				inProgress = false;
+			}, 100
+		);
 	}
 );
